729a642fb6db93eace96f4d173d6d208e99cf804
Le Dinh Hau (BD/PTD-TAF13) <Hau.LeDinh@vn.bosch.com>
2026-06-25 18:55:08 +0700

    fix: Provide worker and chunk files for document viewers and use fetch for WASM

---
+++ web-app-corp/public/src-2FP7IUE6.js
@@ -0,0 +1,16 @@
+import {
+  createMinimalDocxPackage,
+  getPart,
+  packageToArrayBuffer,
+  parseDocx,
+  withPart
+} from "./chunk-XBI2PYTU.js";
+import "./chunk-WKBPLHUA.js";
+export {
+  createMinimalDocxPackage,
+  getPart,
+  packageToArrayBuffer,
+  parseDocx,
+  withPart
+};
+//# sourceMappingURL=src-2FP7IUE6.js.map
