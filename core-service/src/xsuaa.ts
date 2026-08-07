d9f40aea7a41f1e47c7b13e9f4ab45351c0ba26f
Nguyen Dac Hien (BD/PTD-TAF13) <iyh1hc@bosch.com>
2026-08-07 14:03:30 +0700

    CW32

--- core-service/src/xsuaa.ts
+++ core-service/src/xsuaa.ts
@@ -20,7 +20,7 @@ let xsuaaService: any | undefined;
 let createSecurityContext: ((service: any, options: { token: string }) => Promise<unknown>) | undefined;
 
 export function isXsuaaEnabled(): boolean {
-	return process.env.SYMPHONY_STUDIO_EDGE_AUTH === "xsuaa";
+	return process.env.OCTO_EDGE_AUTH === "xsuaa";
 }
 
 async function ensureService(): Promise<boolean> {
